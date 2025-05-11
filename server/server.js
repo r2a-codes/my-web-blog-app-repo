52ad789e9b33173aa18b3dfd8a4f70182235603a
r2a-codes <99102633+r2a-codes@users.noreply.github.com>
2025-05-11 19:12:59 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ server.use(cookie());
 
 server.use(
   cors({
-    origin: process.env.CLIENT_URL ,
+    origin: [process.env.CLIENT_URL,"http://localhost:5173","http://127.0.0.1:5173"],
     credentials: true,
   })
 );
